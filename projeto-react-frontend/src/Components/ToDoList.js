6e8ecde7db5833faa2e8e9a91fa7169d9118fe94
Carol0106 <61694944+Carol0106@users.noreply.github.com>
2024-06-11 16:32:23 -0300

    ajustes

--- projeto-react-frontend/src/Components/ToDoList.js
+++ projeto-react-frontend/src/Components/ToDoList.js
@@ -3,7 +3,7 @@ import '../Assets/css/ToDoList.css';
 
 const ToDoList = () => {
   return (
-    <section className="ToDoList-section">
+    <section className="ToDoList-section" >
       <h2 className='underline-colorido underline-colorido-spaced'>To-do List</h2>
       <p>Drag and drop to set your main priorities, check <br/> when done and create what´s new.</p>
     </section>
